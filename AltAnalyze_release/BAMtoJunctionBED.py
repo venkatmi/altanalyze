451d5e874adcd4824c7d4d56779e1554d6ad2148
nsalomonis <nsalomonis@d8f6aafc-6c53-0410-93e1-47da3a18599f>
2015-08-24 15:37:11 +0000

    Version 2.0.9.2 - Final code.google commit

---
+++ AltAnalyze_release/BAMtoJunctionBED.py
@@ -0,0 +1,190 @@
+###BAMtoJunctionBED
+#Author Nathan Salomonis - nsalomonis@gmail.com
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy 
+#of this software and associated documentation files (the "Software"), to deal 
+#in the Software without restriction, including without limitation the rights 
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+#copies of the Software, and to permit persons to whom the Software is furnished 
+#to do so, subject to the following conditions:
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+#PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""This script can be run on its own to extract a single BAM file at a time or
+indirectly by multiBAMtoBED.py to extract junction.bed files (Tophat format)
+from many BAM files in a single directory at once. Currently uses the Tophat
+predicted Strand notation opt('XS') for each read. This can be substituted with
+strand notations from other aligners (check with the software authors)."""
+
+import pysam
+import string,os,sys,copy,getopt
+import time
+
+def getSpliceSites(cigarList,X):
+    cummulative=0
+    coordinates=[]
+    for (code,seqlen) in cigarList:
+        if code == 0:
+            cummulative+=seqlen
+        if code == 3:
+            #if strand == '-':
+            five_prime_ss = str(X+cummulative)
+            cummulative+=seqlen  ### add the intron length
+            three_prime_ss = str(X+cummulative+1) ### 3' exon start (prior exon splice-site + intron length)
+            coordinates.append([five_prime_ss,three_prime_ss])
+            up_to_intron_dist = cummulative
+    return coordinates, up_to_intron_dist
+
+def writeJunctionBedFile(junction_db,jid,o):
+    strandStatus = True
+    for (chr,jc,tophat_strand) in junction_db:
+        if tophat_strand==None:
+            strandStatus = False
+        break
+        
+    if strandStatus== False: ### If no strand information in the bam file filter and add known strand data
+        junction_db2={}
+        for (chr,jc,tophat_strand) in junction_db:
+            for j in jc:                
+                try:
+                    strand = splicesite_db[chr,j]
+                    junction_db2[(chr,jc,strand)]=junction_db[(chr,jc,tophat_strand)]
+                except Exception: pass
+        junction_db = junction_db2
+        
+
+    for (chr,jc,tophat_strand) in junction_db:
+        x_ls=[]; y_ls=[]; dist_ls=[]
+        read_count = str(len(junction_db[(chr,jc,tophat_strand)]))
+        for (X,Y,dist) in junction_db[(chr,jc,tophat_strand)]:
+            x_ls.append(X); y_ls.append(Y); dist_ls.append(dist)
+        outlier_start = min(x_ls); outlier_end = max(y_ls); dist = str(max(dist_ls))
+        exon_lengths = outlier_start
+        exon_lengths = str(int(jc[0])-outlier_start)+','+str(outlier_end-int(jc[1])+1)
+        junction_id = 'JUNC'+str(jid)+':'+jc[0]+'-'+jc[1] ### store the unique junction coordinates in the name
+        output_list = [chr,str(outlier_start),str(outlier_end),junction_id,read_count,tophat_strand,str(outlier_start),str(outlier_end),'255,0,0\t2',exon_lengths,'0,'+dist]
+        o.write(string.join(output_list,'\t')+'\n')
+          
+            
+def retreiveAllKnownSpliceSites():
+    ### Uses a priori strand information when none present
+    import export, unique
+    parent_dir = export.findParentDir(bam_file)
+    for file in os.listdir(parent_dir):
+        if 'AltAnalyze_report' in file and '.log' in file:
+            log_file = unique.filepath(parent_dir+'/'+file)
+            log_contents = open(log_file, "rU")
+            species_tag = '	species: '
+            for line in log_contents:
+                line = line.rstrip()
+                if species_tag in line:
+                    species = string.split(line,species_tag)[1]
+    splicesite_db={}
+    refExonCoordinateFile = unique.filepath('AltDatabase/ensembl/'+species+'/'+species+'_Ensembl_exon.txt')
+    firstLine=True
+    for line in open(refExonCoordinateFile,'rU').xreadlines():
+        if firstLine: firstLine=False
+        else:
+            line = line.rstrip('\n')
+            t = string.split(line,'\t'); #'gene', 'exon-id', 'chromosome', 'strand', 'exon-region-start(s)', 'exon-region-stop(s)', 'constitutive_call', 'ens_exon_ids', 'splice_events', 'splice_junctions'
+            geneID, exon, chr, strand, start, stop = t[:6]
+            #start = int(start); stop = int(stop)
+            #geneID = string.split(exon,':')[0]
+            splicesite_db[chr,start]=strand
+            splicesite_db[chr,stop]=strand
+    return splicesite_db
+
+def parseJunctionEntries(bam_dir,multi=False):
+    global bam_file
+    global splicesite_db
+    bam_file = bam_dir
+    try: splicesite_db = retreiveAllKnownSpliceSites()
+    except Exception: splicesite_db={}
+    start = time.time()
+    
+    try: import collections; junction_db=collections.OrderedDict()
+    except Exception:
+        try: import ordereddict; junction_db = ordereddict.OrderedDict()
+        except Exception: junction_db={}
+    original_junction_db = copy.deepcopy(junction_db)
+    
+    bamf = pysam.Samfile(bam_dir, "rb" )
+    chromosome = False
+    count=0
+    jid = 1
+    prior_jc_start=0
+    l1 = None; l2=None
+    o = open (string.replace(bam_dir,'.bam','__junction.bed'),"w")
+    o.write('track name=junctions description="TopHat junctions"\n')
+    outlier_start = 0; outlier_end = 0; read_count = 0
+    for entry in bamf.fetch():
+      codes = map(lambda x: x[0],entry.cigar)
+      try: cigarstring = entry.cigarstring
+      except Exception:
+          if 3 in codes: cigarstring = 'N'
+          else: cigarstring = None
+      if cigarstring != None:
+        if 'N' in cigarstring: ### Hence a junction
+            if entry.cigar[0][1]<60 and entry.cigar[0][1]>20:
+                """
+                if count<310:
+                    a1 = entry.seq[entry.cigar[0][1]-5:entry.cigar[0][1]]
+                    a2 = entry.seq[entry.cigar[0][1]:entry.cigar[0][1]+6]
+                    if l1==a1 and l2==a2: continue
+                    else:
+                        print entry.opt('XS'), a1,a2, entry.seq
+                        l1 = a1; l2 = a2
+                else: sys.exit()"""
+                
+            if prior_jc_start == 0: pass
+            elif (entry.pos-prior_jc_start) > 5000 or bamf.getrname( entry.rname ) != chromosome: ### New chr or far from prior reads
+                writeJunctionBedFile(junction_db,jid,o)
+                junction_db = copy.deepcopy(original_junction_db) ### Re-set this object
+                jid+=1
+            chromosome = bamf.getrname( entry.rname ) 
+            X=entry.pos
+            Y=entry.pos+entry.alen
+            prior_jc_start = X
+            if entry.is_reverse:
+                strand = '-' ### This is the strand the seq aligns to but not necessarily the REAL strand the mRNA aligns to (see XS below)
+            else:                
+                strand = '+'
+            try: tophat_strand = entry.opt('XS') ### TopHat knows which sequences are likely real splice sites so it assigns a real strand to the read
+            except Exception:
+                #if multi == False:  print 'No TopHat strand information';sys.exit()
+                tophat_strand = None
+            coordinates,up_to_intron_dist = getSpliceSites(entry.cigar,X)
+            for (five_prime_ss,three_prime_ss) in coordinates:
+                jc = five_prime_ss,three_prime_ss
+                #print X, Y, jc, entry.cigarstring, entry.cigar
+                try: junction_db[chromosome,jc,tophat_strand].append([X,Y,up_to_intron_dist])
+                except Exception: junction_db[chromosome,jc,tophat_strand] = [[X,Y,up_to_intron_dist]]
+            count+=1
+    writeJunctionBedFile(junction_db,jid,o) ### One last read-out
+    if multi == False:
+        print time.time()-start, 'seconds required to parse the BAM file'
+    o.close()
+    bamf.close()
+
+if __name__ == "__main__":
+    ################  Comand-line arguments ################
+    if len(sys.argv[1:])<=1:  ### Indicates that there are insufficient number of command-line arguments
+        print "Warning! Please designate a BAM file as input in the command-line"
+        print "Example: python BAMtoJunctionBED.py --i /Users/me/sample1.bam"
+        sys.exit()
+    else:
+        options, remainder = getopt.getopt(sys.argv[1:],'', ['i='])
+        for opt, arg in options:
+            if opt == '--i': bam_dir=arg ### full path of a BAM file
+            else:
+                print "Warning! Command-line argument: %s not recognized. Exiting..." % opt; sys.exit()
+            
+    try: parseJunctionEntries(bam_dir)
+    except ZeroDivisionError:
+        print [sys.argv[1:]],'error'; error
+
---
+++ AltAnalyze_release/SashimiPlot.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+
+import numpy as np
+import pylab as pl
+import sys,string
+import os
+import misopy
+from misopy.sashimi_plot import sashimi_plot as ssp
+import subprocess
+import multiprocessing
+import time
+import subprocess
+import random
+from pyPdf import PdfFileReader, PdfFileWriter
+import argparse
+import math
+import unique
+import traceback
+count_sum_array=[];
+dem=dict()
+sample_read=dict()
+gene_label=[]
+gene_sym=dict()
+
+def genelist(fname):
+    fname = unique.filepath(fname)
+    lis=[]
+    for line in open(fname,'rU').xreadlines():
+        line = cleanUpLine(line)
+        t=string.split(line,'\t')
+	gene=string.split(t[2],':')
+	val=t[2]+' '+t[3]
+	lis.append(val)
+	
+        if gene[0] in gene_sym:
+		continue	
+	else:
+		gene_sym[gene[0]]=t[0]
+	       
+	
+        #print t[0]
+    return lis,gene_sym
+
+def sample(fname):
+    fname = unique.filepath(fname)
+    head=0
+    samplelis=[]
+    for line in open(fname,'rU').xreadlines():
+        line = cleanUpLine(line)
+	if head ==0:
+	    t=string.split(line,'\t')
+	    #print t
+	    for p in range(11,len(t)):
+		samplelis.append(t[p])
+	    head=1
+        else:
+	    break;
+    return samplelis
+
+def cleanUpLine(line):
+    line = string.replace(line,'\n','')
+    line = string.replace(line,'\c','')
+    data = string.replace(line,'\r','')
+    data = string.replace(data,'"','')
+    return data
+
+def update_plot_settings(bamdir,list1,list2,samp):
+    export_pl=open(unique.filepath('Config/sashimi_plot_settings.txt'),'w')
+    export_pl.write('[data]')
+    export_pl.write('\n')
+    export_pl.write('bam_prefix = '+bamdir+'\n')
+    export_pl.write('bam_files =[')
+  
+    for i in range(len(list1)):
+        g=samp[list1[i]].replace('.bed','.bam')
+	#print i
+        if i==len(list1)-1 and len(list2)==0:
+            export_pl.write('"'+g+'"]')
+        else:
+            export_pl.write('"'+g+'",')      
+    for j in range(len(list2)):
+	#print j
+        g=samp[list2[j]].replace('.bed','.bam')
+        export_pl.write('"'+g+'"')
+        if j==len(list2)-1:
+            export_pl.write(']')
+        else:
+            export_pl.write(',')
+    
+	
+    export_pl.write('\n')
+    export_pl.write('[plotting]')
+    export_pl.write('\n') 
+    export_pl.write('fig_width = 7 \nfig_height = 7 \nintron_scale = 30 \nexon_scale = 4 \nlogged = False\n')
+    export_pl.write('font_size = 6 \nbar_posteriors = False \nnyticks = 4 \nnxticks = 4 \n')
+    export_pl.write('show_ylabel = False \nshow_xlabel = True \nshow_posteriors = False \nnumber_junctions = True \n')
+    export_pl.write('resolution = .5 \nposterior_bins = 40 \ngene_posterior_ratio = 5 \n')
+    export_pl.write('colors =[')
+    for i in range(len(list1)):
+        export_pl.write('"'+'red'+'"')
+        if i==len(list1)-1 and len(list2)==0:
+            export_pl.write(']')
+        else:
+            export_pl.write(',')
+    for j in range(len(list2)):
+        export_pl.write('"'+'blue'+'"')
+        if j==len(list2)-1:
+            export_pl.write(']')
+        else:
+            export_pl.write(',')
+    export_pl.write('\n')       
+    export_pl.write('coverages =[')
+    for i in range(len(list1)):
+        
+        e=sample_read[samp[list1[i]]]
+        export_pl.write(str(int(e)))
+        if i==len(list1)-1 and len(list2)==0:
+            export_pl.write(']')
+        else:
+            export_pl.write(',')
+    for j in range(len(list2)):
+        e=sample_read[samp[list2[j]]]
+        export_pl.write(str(int(e)))
+        if j==len(list2)-1:
+            export_pl.write(']')
+        else:
+            export_pl.write(',')
+    export_pl.write('\n')
+    export_pl.write('bar_color = "b" \nbf_thresholds = [0, 1, 2, 5, 10, 20]')
+    export_pl.close()
+        
+
+def sashmi_plot_list(bamdir,fname,gene_label,lines,samp,gene_sym):
+    splicing_events=[]
+    type = None
+    firstLine = True
+    for line in open(fname,'rU').xreadlines():
+	line = cleanUpLine(line)
+	t = string.split(line,'\t')
+	if firstLine:
+	    if 'junctionID-1' in t:
+		j1i = t.index('junctionID-1')
+		j2i = t.index('junctionID-2')
+		type='ASPIRE'
+	    if 'ANOVA' in t:
+		type='PSI'
+	    elif 'independent confirmation' in t:
+		type=='confirmed'
+	    firstLine=False
+	if ' ' in t[0] and ':' in t[0]:
+	    splicing_events.append(t[0])
+	elif type=='ASPIRE':
+	    splicing_events.append(t[j1i] +' '+ t[j2i])
+	elif type=='PSI':
+	    try:
+		j1,j2 = string.split(t[0],'|')
+		a,b,c = string.split(j1,':')
+		j1 = b+':'+c
+		splicing_events.append(j1 +' '+ j2)
+	    except Exception: pass
+	elif type=='confirmed':
+	    try:
+		event_pair1 = string.split(t[1],'|')[0]
+		a,b,c,d = string.split(event_pair1,'-')
+		splicing_events.append(a+'-'+b +' '+ c+'-'+d)
+	    except Exception: pass
+
+    if len(splicing_events)==0:
+	forceNoCompatibleEventsInFile
+    
+    print 'Exporting plots',
+    for li in splicing_events:
+	if ":U" in li or "-U" in li:
+	    
+	    continue
+	else:
+	
+	 li=cleanUpLine(li)
+	 #print li
+	 
+	#dem[0]=['ENSG00000132424:I10.1 ENSG00000132424:E10.1-E11.1','ENSG00000146147:E10.3-E11.1 ENSG00000146147:E9.3-E15.1']
+	 de=string.split(li,'\t')
+	 dem[0]=de
+	 #print dem[0]
+	 for key in dem:
+	  for i in range(len(dem[key])):
+	    list1=[]
+	    list2=[]
+	    try:
+		k=gene_label.index(dem[key][i])
+		flag=1
+		lt=cleanUpLine(lines[k])
+		t=string.split(lt,'\t')
+		#print t
+		t=t[11:]
+		#print t
+		#list3=[]
+		#ind=[]
+		for x in range(len(t)):
+		    #print x,t[x]
+		    if(t[x]!=''):
+			if float(t[x]) < 0.8:
+			    list1.append(x)
+			    #print x
+			    #print 'list1:'+str(x)
+			else:
+			    list2.append(x)
+			    #print x
+			   # print str(x)
+		     
+		    else:
+			continue
+	    
+		if len(list1)>5:
+		    list1=list1[1:5]
+		if len(list2)>5:
+		    list2=list2[1:5]
+		#print len(list1),len(list2)
+	    except Exception:
+		
+		for ij in range(len(samp)):
+		    list1.append(ij)
+	    
+	    update_plot_settings(bamdir,list1,list2,samp)
+	    
+	    a=string.split(dem[key][i]," ")
+	    if '-' in a[1]:
+		    
+		    ch1=a[1]
+		    f=string.split(a[0],':')
+	    else:
+		    ch1=a[0]
+		    f=string.split(a[1],':')
+	    event=findParentDir(inputpsi)
+	    event=event+"trial_index/"
+	    setting =unique.filepath("Config/sashimi_plot_settings.txt")
+	    name=ch1
+	    #outputdir=findParentDir(inputpsi)+"sashimiplots"
+	    try: os.makedirs(outputdir)
+	    except Exception: pass
+	    
+	try:
+	    ssp.plot_event(ch1,event,setting,outputdir)
+	except Exception:
+	    #print traceback.format_exc()
+	    #print "error2"
+	    continue
+    #outputdir=findParentDir(inputpsi)+"sashimiplots" 
+    for filename in os.listdir(outputdir):
+	newname=string.split(filename,'/')
+	#print newname[0]
+	if newname[0] in gene_sym:
+	    new_path = gene_sym[newname[0]]+'-'+filename
+	    #new_path = string.replace()
+	    os.rename(filename,new_path)
+	else:
+	    continue
+
+  
+def findParentDir(filename):
+    filename = string.replace(filename,'//','/')
+    filename = string.replace(filename,'\\','/')
+    x = string.find(filename[::-1],'/')*-1
+    return filename[:x]      
+
+def Sashimiplottting(bamdir,countsin,inputpsi,genelis):
+    inputpsi = unique.filepath(inputpsi)
+    text_file = open(inputpsi,'rU')
+    lines = text_file.readlines()
+   
+    text_file.close()
+    samp=sample(inputpsi)
+    gene_label,gene_sym=genelist(inputpsi)
+
+    header=True
+    junction_max=[]
+    countsin = unique.filepath(countsin)
+    for line in open(countsin,'rU').xreadlines():
+        data = cleanUpLine(line)
+        t = string.split(data,'\t')
+        if header:
+            samples = t[1:]
+            header=False
+            exon_sum_array=[0]*len(samples)
+            count_sum_array=[0]*len(samples)
+        else:
+            values = map(float,t[1:])
+            count_sum_array = [sum(value) for value in zip(*[count_sum_array,values])]
+
+        for i in range(len(samp)):
+	   sample_read[samp[i]]=count_sum_array[i]
+          #print samp[i],sample_read[samp[i]]
+
+    genelis = unique.filepath(genelis)
+
+    sashmi_plot_list(bamdir,genelis,gene_label,lines,samp,gene_sym)
+
+def remoteSashimiPlot(species,fl,bamdir,genelis):
+    global inputpsi
+    global outputdir
+    try:
+	countinp = fl.CountsFile()
+	root_dir = fl.RootDir()
+    except Exception:
+	root_dir = fl
+        search_dir = root_dir+'/ExpressionInput'
+        files = unique.read_directory(search_dir)
+        for file in files:
+            if 'counts.' in file and 'steady-state.txt' not in file:
+                    countinp = search_dir+'/'+file
+		    
+    inputpsi = root_dir+'/AltResults/AlternativeOutput/'+species+'_RNASeq_top_alt_junctions-PSI.txt'
+    #outputdir=findParentDir(inputpsi)+"sashimiplots"
+    outputdir = root_dir+'/ExonPlots'
+    outputdir = root_dir+'/SashimiPlots'
+    try: os.mkdir(unique.filepath(outputdir))
+    except Exception: pass
+    #print bamdir
+    #print countinp
+    #print inputpsi
+    #print genelis
+    Sashimiplottting(bamdir,countinp,inputpsi,genelis)
+
+    gene_label,gene_sym=genelist(inputpsi)
+    for filename in os.listdir(outputdir):
+	if '.pdf' in filename:
+	    newname=string.split(filename,':')
+	    if newname[0] in gene_sym:
+		new_filename = str(filename)
+		if ':' in filename:
+		    new_filename = string.split(filename,':')[1]
+		elif '\\' in filename:
+		    new_filename = string.split(filename,'\\')[1]
+		elif '/' in filename:
+		    new_filename = string.split(filename,'/')[1]
+	        nnname=gene_sym[newname[0]]+'-SashimiPlot_'+new_filename
+		os.rename(os.path.join(outputdir, filename), os.path.join(outputdir,nnname))
+	    else:
+		continue
+
+if __name__ == '__main__':
+    root_dir = '/Volumes/salomonis1/projects/Bex1-RIP/Input/AltAnalyze_new'
+    genelis = '/Volumes/salomonis1/projects/Bex1-RIP/Input/AltAnalyze_new/AltResults/AlternativeOutput/Rn_RNASeq_top_alt_junctions-PSI-clust-ANOVA.txt'
+    genelis = '/Volumes/salomonis1/projects/Bex1-RIP/Input/AltAnalyze_new/AltResults/Clustering/top50/Combined-junction-exon-evidence.txt'
+    bamdir = '/Volumes/salomonis1/projects/Bex1-RIP/Input/bams'
+    remoteSashimiPlot('Rn',root_dir,bamdir,genelis)
+    sys.exit()
